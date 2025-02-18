fc7533e694d268f87e5c006aade9bea90e5eb4c4
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2025-02-18 23:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function incrementBtn() {
-    let counterElement = document.getElementById("counter"); 
+// Select the button and counter elements
+let incrementButton = document.getElementById("incrementBtn");
+let counterElement = document.getElementById("counter");
+
+// Add a click event listener to the button
+incrementButton.addEventListener("click", function () {
     let currentValue = parseInt(counterElement.innerText); // Get current counter value
 
     alert("Current value before increment: " + currentValue); // Alert un-incremented value
 
     counterElement.innerText = currentValue + 1; // Increment counter
-}
+});
