0a68025c74f1ecc64b4706c39be38cd3195b7781
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2025-02-18 23:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function incrementBtn() {
+    let counterElement = document.getElementById("counter");
+    let currentValue = parseInt(counterElement.innerText); // Get current counter value
+
+    alert("Current value before increment: " + currentValue); // Alert un-incremented value
+
+    counterElement.innerText = currentValue + 1; // Increment counter
+}
