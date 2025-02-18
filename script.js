f46a8091d6dd7fd92ace92e4764ef4cb88145cd2
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2025-02-18 23:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// Get the button element correctly (without the `#` symbol)
-let incrementButton = document.getElementById("incrementBtn");
-
-// Add an event listener to trigger the function on button click
-incrementButton.addEventListener("click", function () {
+function incrementBtn() {
     let counterElement = document.getElementById("counter"); 
     let currentValue = parseInt(counterElement.innerText); // Get current counter value
 
     alert("Current value before increment: " + currentValue); // Alert un-incremented value
 
     counterElement.innerText = currentValue + 1; // Increment counter
-});
+}
